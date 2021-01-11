a5919ce6264187001a946d058ba59c44b5800bb9
Manu Thambi <manu@meshcapital.com>
2021-01-11 09:38:06 -0500

    Run the build fail tests only on compiler versions: beta, stable:1.49+

--- big_enum_set/tests/trybuild.rs
+++ big_enum_set/tests/trybuild.rs
@@ -1,7 +1,21 @@
-#[rustversion::any(stable, beta)]
+use once_cell::sync::Lazy;
+use std::sync::Mutex;
+use trybuild::TestCases;
+
+// Fail tests are very sensitive to the rust compiler version.
+#[rustversion::any(all(stable, since(1.49)), beta)]
 #[test]
-fn ui() {
-    let t = trybuild::TestCases::new();
+fn ui_fail() {
+    let t = TEST_CASES.lock().unwrap();
     t.compile_fail("tests/compile-fail/*.rs");
+}
+
+#[test]
+fn ui_pass() {
+    let t = TEST_CASES.lock().unwrap();
     t.pass("tests/compile-pass/*.rs");
 }
+
+// Keep singleton TestCases. Using multiple harnesses simultaneously have problems.
+// https://github.com/dtolnay/trybuild/issues/58
+static TEST_CASES: Lazy<Mutex<TestCases>> = Lazy::new(|| Mutex::new(TestCases::new()));
