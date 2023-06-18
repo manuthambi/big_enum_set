6207e30451a9a069ac9ba9b607a12857817bc1a9
Manu Thambi <manu@meshcapital.com>
2023-06-18 00:29:30 -0400

    Add tests for the iteration order of enumsets.

--- big_enum_set/tests/ops.rs
+++ big_enum_set/tests/ops.rs
@@ -245,6 +245,17 @@ macro_rules! test_enum {
         }
 
         #[test]
+        fn iter_ordering_test() {
+            let set_a = $e::A | $e::B | $e::E;
+            let vec_a: Vec<_> = set_a.iter().collect();
+            assert_eq!(vec_a, &[$e::A, $e::B, $e::E]);
+
+            let set_a = $e::B | $e::D | $e::G;
+            let vec_a: Vec<_> = set_a.iter().collect();
+            assert_eq!(vec_a, &[$e::B, $e::D, $e::G]);
+        }
+
+        #[test]
         fn test_iter_size_hint() {
             fn check_iter_size_hint(set: BigEnumSet<$e>) {
                 let count = set.len();
